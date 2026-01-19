f06de4576856de439859dec5501b95a67207b035
lishijie36 <lishijie36@meituan.com>
2026-01-19 19:19:04 +0800

    主题色切换

--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const Navbar = () => {
   const isScrolled = useScrollTop();
@@ -14,6 +15,9 @@ export const Navbar = () => {
       )}
     >
       <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        <ModeToggle/>
+      </div>
     </div>
   );
 };
