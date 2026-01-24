009f01c3be32ad016e2e2e7cc55de03ec2711de3
HaveNiceDa <2676424029@qq.com>
2026-01-24 20:10:06 +0800

    '测试'

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
+      domain: "https://whole-badger-19.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
