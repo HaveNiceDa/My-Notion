8a12fa367a49cd8e1d2784b34091e58d2ef62a42
HaveNiceDa <2676424029@qq.com>
2026-04-14 22:35:04 +0800

    feat: 收敛mobile的convex

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,9 +1,7 @@
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProvider } from "convex/react";
 import { Stack } from "expo-router";
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-});
+import { convex } from "@/app/src/lib/convex";
 
 export default function RootLayout() {
   return (
--- apps/mobile/app/src/lib/convex/index.ts
+++ apps/mobile/app/src/lib/convex/index.ts
@@ -1,3 +1,8 @@
-import { initConvexClient } from "@notion/convex/client";
+import { ConvexReactClient } from "convex/react";
 
-export const convex = initConvexClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
+export const convex = new ConvexReactClient(
+  process.env.EXPO_PUBLIC_CONVEX_URL!,
+  {
+    unsavedChangesWarning: false,
+  },
+);
