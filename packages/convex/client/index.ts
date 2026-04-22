f54b457dafe5c7b5b4821315011dc83f6cae1adf
lishijie.36 <lishijie.36@bytedance.com>
2026-04-22 10:42:01 +0800

    refactor: 将TypeScript目标版本升级至es2017并修改私有字段语法
    将web应用的TypeScript编译目标从es5升级到es2017以获得更好的现代JavaScript特性支持
    同时将ConvexClient中的私有字段语法从#前缀改为private修饰符以提升兼容性

--- packages/convex/client/index.ts
+++ packages/convex/client/index.ts
@@ -1,14 +1,14 @@
 import { ConvexReactClient } from "convex/react";
 
 export class ConvexClient {
-  static #client: ConvexReactClient | null = null;
+  private static client: ConvexReactClient | null = null;
 
   static getClient(url: string) {
-    if (!this.#client) {
-      this.#client = new ConvexReactClient(url, {
+    if (!this.client) {
+      this.client = new ConvexReactClient(url, {
         unsavedChangesWarning: false,
       });
     }
-    return this.#client;
+    return this.client;
   }
 }
